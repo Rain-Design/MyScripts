8592ea90f6a7244d72153ce479d1babb3df0f62a
Shaddowzinn <84105617+Rain-Design@users.noreply.github.com>
2022-07-04 08:02:27 -0300

    Update AutofarmModule.lua

--- AutofarmModule.lua
+++ AutofarmModule.lua
@@ -1,12 +1,16 @@
 local Module = {}
 
+local Services = {
+    TweenService = game:GetService("TweenService"),
+    RunService = game:GetService("RunService")
+}
+
 function Module:Check()
 
 local Character = game.Players.LocalPlayer.Character
-
-if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Humanoid") and Character.Humanoid.Health > 0 then
-    return true
-end
+    if Character and Character:FindFirstChild("HumanoidRootPart") and Character:FindFirstChild("Humanoid") and Character.Humanoid.Health > 0 then
+        return true
+    end
 
 return false
 end
@@ -30,9 +34,34 @@ function Module:Tween(Pos,Speed)
 local Character,HumanoidRootPart = Module:GetCharacter()
 
 local Distance = Module:GetDistance(Pos)
-local MyTween = game:GetService("TweenService"):Create(HumanoidRootPart,TweenInfo.new(Distance / Speed),{CFrame = Pos})
+local MyTween = Services.TweenService:Create(HumanoidRootPart,TweenInfo.new(Distance / Speed),{CFrame = Pos})
 
 return MyTween, Distance
+end
+
+function Module:Noclip()
+
+local Character = Module:GetCharacter()
+coroutine.wrap(function()
+    Services.RunService:RenderStepped:Connect(function()
+        for _,v in pairs(Character:GetChildren()) do
+            if v.ClassName == "BasePart" then
+                v.CanCollide = false
+            end
+        end
+    end)
+end)()
+end
+
+function SetVelocity()
+
+local Character,HumanoidRootPart = Module:GetCharacter()
+
+coroutine.wrap(function()
+    Services.RunService:RenderStepped:Connect(function()
+        HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+    end)
+end)()
 end
 
 return Module
