3d3c0656397a052d4aaacd8dae6f18cd666c3813
Shaddowzinn <84105617+Rain-Design@users.noreply.github.com>
2022-12-17 00:06:06 -0300

    Create FruitsBattleground.lua

---
+++ FruitsBattleground.lua
@@ -0,0 +1,49 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local teleportService = game:GetService("TeleportService")
+local players = game:GetService("Players")
+local httpService = game:GetService("HttpService")
+
+local replicator = replicatedStorage:WaitForChild("Replicator")
+local player = players.LocalPlayer
+local mainData = player:WaitForChild("MAIN_DATA")
+local gems = mainData:WaitForChild("Gems")
+
+local Settings = httpService:JSONDecode(readfile("fruitFolder/Settings.json"))
+
+replicator:InvokeServer("FruitsHandler", "SwitchSlot", {Slot = Settings.Slot})
+task.wait(.15)
+replicator:InvokeServer("Core", "UpdateSettings", {["\255"] = true, ["\255"] = true, ["\255"] = true, ["\255"] = true, ["\255"] = true})
+
+while true do
+    if gems.Value < 80 then
+        syn.queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/Rain-Design/MyScripts/main/FruitsBattleground.lua", true))()]])
+        teleportService:Teleport(game.PlaceId, player)
+        teleportService.TeleportInitFailed:Connect(function()
+            rconsolewarn(string.format("Error while trying to teleport. Retrying."))
+            teleportService:Teleport(game.PlaceId, player)
+        end)
+        rconsolewarn("Teleported.")
+        break
+    end
+    
+    local fruit = replicator:InvokeServer("FruitsHandler", "Spin", {Type = "Fast"})
+    local currentTime = os.date("%X")
+    
+    if fruit == nil then
+        continue
+    end
+    
+    if table.find(Settings.Wanted, fruit) then
+        rconsolewarn(string.format("%s - Congrats! you got: %s", currentTime, fruit))
+        replicator:InvokeServer("Core", "UpdateSettings", {Textures = true, FPSCounter = true, KillFeed = true, Music = true, Shadows = true})
+        break
+    end
+    
+    rconsolewarn(string.format("%s - You got: %s", currentTime, fruit))
+    
+    task.wait(.2)
+end
